cd68f6f626aaeffc2efa88b8b3483ac92d8c9646
devferx <ferkki@hotmail.com>
2020-12-02 13:09:45 -0400

    [add] form actions

--- components/Form/index.jsx
+++ components/Form/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { FormContainer } from './styles'
 import { Header } from '../Header'
 import { PricingSelector } from '../PricingSelector'
+import { FormContainer, Input, Button } from './styles'
 
 export const Form = () => {
   return (
     <FormContainer>
       <Header />
       <PricingSelector />
+      <Input type="text" placeholder="Nombre" />
+      <Input type="text" placeholder="nombre@correo.com" />
+      <Button type="submit">Quiero regístrarme</Button>
     </FormContainer>
   )
 }
--- components/Form/styles.js
+++ components/Form/styles.js
@@ -3,4 +3,38 @@ import styled from '@emotion/styled'
 export const FormContainer = styled.form`
   padding: 16px;
   box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+`
+
+export const Input = styled.input`
+  padding: 1rem;
+  background-color: #f5f0ec;
+  border-radius: 8px;
+  margin-top: 1rem;
+  font-size: 14px;
+  border: 1px solid #dbd4d5;
+
+  &:focus {
+    outline-color: #7e43b8;
+  }
+`
+
+export const Button = styled.button`
+  width: 100%;
+  background-color: #98ca3f;
+  color: white;
+  padding: 1rem;
+  margin-top: 1rem;
+  font-weight: 700;
+  font-size: 16px;
+  border-radius: 8px;
+  cursor: pointer;
+  text-transform: uppercase;
+  border: none;
+  outline: none;
+
+  &:hover {
+    background-color: #c1df8b;
+  }
 `
